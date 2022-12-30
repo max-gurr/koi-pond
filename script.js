185e4c2b395216f1ca3ae49c91680a402505e44f
Angstylemon <maxd.gurr@gmail.com>
2022-12-30 16:44:07 +0100

    Push away from edges

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ let canvas;
 let ctx;
 let f;
 let width, height;
-let mouseX, mouseY;
 
 window.addEventListener("resize", function() {init();})
 
@@ -17,12 +16,8 @@ function init() {
 	canvas.width = width;
 	canvas.height = height;
 
-
-	mouseX = width/2;
-	mouseY = height/2;
-
 	// Make fish
-	f = new Fish(ctx, 300, 300);
+	f = new Fish(ctx, width/2, height/2);
 }
 
 function animate() {
@@ -46,14 +41,29 @@ class Fish {
 	ax;
 	ay;
 
+	maxVel;
+	maxForce;
+
 	constructor(c, x, y) {
 		this.ctx = c
-		
+		this.maxVel = 0.5;
+		this.maxForce = 0.1;
+
+		// Init with random vel
+		this.vx = Math.random()*2-1;
+		this.vy = Math.random()*2-1;
+
+		// Set vel magnitude
+		this.constrainVel();
+
+		// Calc angle of randomised vel
+		let angle = Math.atan2(this.vy, this.vx) - Math.PI;
+		// Make seg using angle
+		let seg1 = new Segment(c, x, y, 10, angle);
+
 		this.segs = [];
-		this.segs.push(new Segment(c, x, y, 10, 0));
-		
-		this.vx = Math.random();
-		this.vy = Math.random(); 
+		this.segs.push(seg1);
+
 		this.ax = 0;
 		this.ay = 0;
 	}
@@ -65,9 +75,29 @@ class Fish {
 
 		// this.ax += dmouseX;
 		// this.ay += dmouseY;
+		let border = 100;
+		let x = this.segs[0].ax;
+		let y = this.segs[0].ay;
+
+		// Check x vs y edges separately to avoid quick turning
+		if (x < border || (width - x) < border) {
+			let targetX = width/2;
+			let dx = targetX - x;
+			this.ax += dx - this.vx;
+		}
+		if (y < border || (height - y) < border) {
+			let targetY = height/2;
+			let dy = targetY - y;
+			this.ay += dy - this.vy;
+		}
+		// Limit acc force
+		this.constrainAcc();
 
 		this.vx += this.ax;
 		this.vy += this.ay;
+
+		// Constrain vel magnitude
+		this.constrainVel();
 
 		this.segs[0].update(this.vx, this.vy);
 		
@@ -79,6 +109,24 @@ class Fish {
 		this.segs.forEach(p => {
 			p.draw();
 		});
+	}
+
+	constrainVel() {
+		let max = this.maxVel;
+		let mag = Math.sqrt(this.vx * this.vx + this.vy * this.vy);
+		
+		this.vx = max * this.vx / mag
+		this.vy = max * this.vy / mag;
+	}
+
+	constrainAcc() {
+		let max = this.maxForce;
+		let mag = Math.sqrt(this.ax * this.ax + this.ay * this.ay);
+		
+		if (mag > max) {
+			this.ax = max * this.ax / mag
+			this.ay = max * this.ay / mag;
+		}
 	}
 }
 
