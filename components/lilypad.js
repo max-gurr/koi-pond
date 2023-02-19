dc6fc252b6aa0cd1397520ad8df576afbdca0db4
max-gurr <maxd.gurr@gmail.com>
2023-02-19 21:55:53 +1300

    Better encapsulation

--- components/lilypad.js
+++ components/lilypad.js
@@ -1,6 +1,8 @@
 class LilyPad extends HTMLElement {
 	constructor() {
 		super();
+		
+    this.attachShadow({mode: 'open'});
 	}
 	
 	connectedCallback() {
@@ -8,7 +10,7 @@ class LilyPad extends HTMLElement {
 	}
 
 	static get observedAttributes() {
-		return ['notchSize', 'rotate'];
+		return ['notchSize', 'rotate', 'rotateBy'];
 	}
 
 	attributeChangedCallback(property, oldValue, newValue) {
@@ -19,20 +21,39 @@ class LilyPad extends HTMLElement {
 
 	render() {
 		const notchSize = parseFloat(this.getAttribute('notchSize')) || 0;
-		const rotate = this.getAttribute('rotate') || 0;
+		const rotate = parseInt(this.getAttribute('rotate')) || 0;
+		const rotateBy = parseInt(this.getAttribute('rotateBy')) || 0;
+		
+		this.shadowRoot.innerHTML = `
+			<style>
+				:host {
+					display: flex;
+					width: 100%;
+					height: 100%;
+					justify-content: center;
+					align-items: center;
+				}
+				svg {
+					position: absolute;
+					top: 0;
+					left: 0;
+					transform: rotate(${rotate}deg);
+					transition: transform 0.25s ease;
+				}
+				svg:hover {
+					transform: rotate(${rotate+rotateBy}deg);
+				}
+				.content {
+					position: relative;
+					z-index: 2;
+					pointer-events: none;
+				}
+			</style>
+			
+			<div class="content">
+				<slot></slot>
+			</div>
 
-		this.style = `
-			display: block;
-			position: absolute;
-			top: 0;
-			left: 0;
-			z-index: -1;
-			width: 100%;
-			height: 100%;
-			transition: transform 0.5s ease;
-		`
-
-		this.innerHTML = `
 			<svg version="1.1" xmlns="http://www.w3.org/2000/svg"
 				viewBox="0 0 100 100">
 		 		<defs>
@@ -55,7 +76,7 @@ class LilyPad extends HTMLElement {
 					</radialGradient>
 				</defs>
  
-				<g mask="url(#cutout)" transform="rotate(${rotate}, 50, 50)">
+				<g mask="url(#cutout)">
 					<circle 
 						cx="50" 
 						cy="50" 
@@ -65,7 +86,7 @@ class LilyPad extends HTMLElement {
 					/>
  
 					<path d="M 50 85 l 0 -20 M 75 75 l -15 -15 M 25 75 l 15 -15 M 15 50 l 20 0 M 85 50 l -20 0 M 25 25 l 15 15 M 75 25 l -15 15 M 20 36 l 20 10 M 60 80 l -8 -20 M 38 78 l 10 -20 M 80 35 l -20 10 M 15 65 l 20 -10 M 87 60 l -25 -6 M 35 15 l 10 20 M 65 18 l -10 20 M 50 15 l 0 25"
-						stroke-width="0.25px"
+						stroke-width="0.7px"
 						stroke="rgb(10, 185, 55)"
 					/>
 				</g>
