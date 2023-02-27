7a574b7523e28852ca9b9c5f70f55c307b0ab520
max-gurr <maxd.gurr@gmail.com>
2023-02-27 21:35:07 +1300

    more lilypad layers and better petal drawing

--- components/lilyflower.js
+++ components/lilyflower.js
@@ -24,7 +24,8 @@ class LilyFlower extends HTMLElement {
 			petals += `
 				<path 
 					d="M 50 50 
-					A ${petal_width} ${petal_width} 0 0 1 50 ${50 - petal_size} 
+					A ${petal_width} ${petal_width} 0 0 1 46 ${60 - petal_size}
+					Q 50 ${50 - petal_size} 54 ${60 - petal_size}
 					A ${petal_width} ${petal_width} 0 0 1 50 50"
 					fill="${petal_fill}"
 					transform="rotate(${rotation}, 50, 50)"
@@ -54,21 +55,32 @@ class LilyFlower extends HTMLElement {
 			<svg version="1.1" xmlns="http://www.w3.org/2000/svg"
 				viewBox="0 0 100 100">
 				<defs>
-					<radialGradient id="outer_petal_fill">
+					<radialGradient id="petal_fill_white">
 						<stop offset="15%" stop-color="rgb(210, 200, 220)" />
 						<stop offset="100%" stop-color="rgb(230, 230, 230)" />
 					</radialGradient>
 
-					<radialGradient id="inner_petal_fill">
-						<stop offset="15%" stop-color="rgb(180, 20, 220)" />
-						<stop offset="100%" stop-color="rgb(210, 200, 220)" />
+					<radialGradient id="petal_fill_pink">
+						<stop offset="15%" stop-color="rgb(218, 186, 198)" />
+						<stop offset="100%" stop-color="rgb(255, 206, 255)" />
+					</radialGradient>
+
+					<radialGradient id="petal_fill_pliurple">
+						<stop offset="15%" stop-color="rgb(228, 126, 198)" />
+						<stop offset="100%" stop-color="rgb(225, 186, 205)" />
+					</radialGradient>
+
+					<radialGradient id="petal_fill_purple">
+						<stop offset="15%" stop-color="rgb(180, 60, 80)" />
+						<stop offset="100%" stop-color="rgb(210, 150, 190)" />
 					</radialGradient>
 				</defs>
 
-				${LilyFlower.generatePetals(50, 46, "url(#outer_petal_fill)", 360/6)}
-				${LilyFlower.generatePetals(45, 46, "url(#outer_petal_fill)", 360/6, 360/12)}
-				${LilyFlower.generatePetals(25, 20, "url(#inner_petal_fill)", 360/9)}
-				${LilyFlower.generatePetals(7, 5, "rgb(215, 255, 39)", 360/10)}
+				${LilyFlower.generatePetals(50, 40, "url(#petal_fill_white)", 360/7)}
+				${LilyFlower.generatePetals(43, 40, "url(#petal_fill_pink)", 360/7, 360/14)}
+				${LilyFlower.generatePetals(33, 30, "url(#petal_fill_pliurple)", 360/9)}
+				${LilyFlower.generatePetals(24, 20, "url(#petal_fill_purple)", 360/11)}
+				${LilyFlower.generatePetals(0, 9, "rgb(215, 255, 39)", 360/13)}
 
 			</svg>
 		`;
